d5d2ca9343610af11cb4f60588b42bb18d744fbc
PabloK09 <pablokollm@gmail.com>
2021-09-18 23:24:28 -0300

    Deploy Back

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,7 +1,8 @@
-export const BASE_URL = 'http://localhost:3001/dogs'
-export const SEARH_ID = 'http://localhost:3001/dogs/'
-export const POST_URL = 'http://localhost:3001/dog'
-export const URL_TEMPERAMENT = 'http://localhost:3001/temperament'
+// export const BASE_URL = 'http://localhost:3001/dogs'
+export const BASE_URL = '/dogs'
+export const SEARH_ID = '/dogs/'
+export const POST_URL = '/dog'
+export const URL_TEMPERAMENT = '/temperament'
 
 export const SEARCH_NAME = BASE_URL + '/search'
-export const CREATE_URL = 'http://localhost:3001/dog'
+export const CREATE_URL = '/dog'
