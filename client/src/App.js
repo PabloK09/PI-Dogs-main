e47f4e9a3860e909ce2839577c14ee8cafd27f25
PabloK09 <pablokollm@gmail.com>
2021-09-19 01:00:36 -0300

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 /* eslint-disable */
 import './App.css';
+import "./index.css";
 import React from 'react'
 import { Route, Switch } from "react-router-dom";
 import LandingPage from './views/landing-page/LandingPage';
